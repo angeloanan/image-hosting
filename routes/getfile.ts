0bce8f3f799b00d2a2284fa1dbfb8b0993f68771
Christopher Angelo <angelo@angeloanan.xyz>
2020-05-03 16:55:13 +0700

    feat(getfile): getting files
    Signed-off-by: Christopher Angelo <angelo@angeloanan.xyz>

---
+++ routes/getfile.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from 'fastify'
+import { getRepository } from 'typeorm'
+import { Image } from '../db/model/Image'
+import fs from 'fs'
+import path from 'path'
+import mime from 'mime-types'
+
+module.exports = function (fastify: FastifyInstance, opts: void, done: VoidFunction): void {
+  fastify.get('/:path', (req, res) => {
+    if (req.params.path == null) res.code(404).send('Not found')
+
+    const urlpath: string = req.params.path
+    const imageRepo = getRepository(Image)
+    imageRepo.findOneOrFail({ urlpath: urlpath })
+      .then(img => {
+        const filename = img.filename
+        const pathToImg = path.join(__dirname, '..', 'img', filename)
+
+        const contentType = mime.lookup(filename) // Will return false if unknown
+        const stream = fs.createReadStream(pathToImg)
+
+        if (contentType === false) return res.code(500).send('Unknown file type')
+        res.code(200).header('Content-Type', contentType).send(stream)
+      }).catch(() => {
+        res.code(404).send('Not found')
+      })
+  })
+  done()
+}
