edb095cf48e8c017fe3cda598045c779ca7db273
Christopher Angelo <angelo@angeloanan.xyz>
2020-05-04 08:50:46 +0700

    feat: appropriate headers
    Included headers:
    * Accept-Ranges: none
    * Content-Disposition: inline
    * Content-Type
    * Content-Length
    * Content-Range
    
    Signed-off-by: Christopher Angelo <angelo@angeloanan.xyz>

--- routes/getfile.ts
+++ routes/getfile.ts
@@ -5,8 +5,13 @@ import fs from 'fs'
 import path from 'path'
 import mime from 'mime-types'
 
+const headers = {
+  'Accept-Ranges': 'none', // TODO: Support partial requests (HTTP 206)
+  'Content-Disposition': 'inline'
+}
+
 module.exports = function (fastify: FastifyInstance, opts: void, done: VoidFunction): void {
-  fastify.get('/:path', (req, res) => {
+  fastify.get('/:path', async (req, res) => {
     if (req.params.path == null) res.code(404).send('Not found')
     if (req.params.path.length < 6) res.code(404).send('Not found') // Hashids minimum 8 chars
 
@@ -17,11 +22,18 @@ module.exports = function (fastify: FastifyInstance, opts: void, done: VoidFunct
         const filename = img.filename
         const pathToImg = path.join(__dirname, '..', 'img', filename)
 
-        const contentType = mime.lookup(filename) // Will return false if unknown
         const stream = fs.createReadStream(pathToImg)
+        const contentType = mime.lookup(filename) // Will return false if unknown
+        const contentLength = fs.statSync(pathToImg, { bigint: true }).size.toString()
 
         if (contentType === false) return res.code(500).send('Unknown file type')
-        res.code(200).header('Content-Type', contentType).send(stream)
+
+        res.code(200)
+          .headers(headers)
+          .header('Content-Type', contentType)
+          .header('Content-Length', contentLength)
+          .header('Content-Range', `bytes */${contentLength}`)
+          .send(stream)
       }).catch(() => {
         res.code(404).send('Not found')
       })
